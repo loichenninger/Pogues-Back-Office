a9448f6c27746ff9d60253c14ecf099f15d50d85
Loïc Henninger <loic.henninger@insee.fr>
2020-11-27 17:26:17 +0100

    Updating annotations on Environment endpoint (swagger v3)

--- src/main/java/fr/insee/pogues/webservice/rest/PoguesEnvironment.java
+++ src/main/java/fr/insee/pogues/webservice/rest/PoguesEnvironment.java
@@ -1,15 +1,5 @@
 package fr.insee.pogues.webservice.rest;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -17,9 +7,26 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.responses.ApiResponse;
+//import io.swagger.v3.oas.annotations.responses.ApiResponses;
+//import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @Component
 @Path("env")
-@Api(value = "Pogues Environment")
+@Tag(name = "Pogues Environment")
 public class PoguesEnvironment {
 
     private final static Logger logger = LogManager.getLogger(PoguesEnvironment.class);
@@ -29,13 +36,10 @@ public class PoguesEnvironment {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @ApiOperation(
-            value = "Get pogues back office environment",
-            notes = "This will return a safe (no secrets) projection of the current backend environment"
-    )
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Success"),
-            @ApiResponse(code = 404, message = "Not found")
+    @Operation(operationId = "getEnvironment", summary="Get pogues back office environment")
+    @ApiResponses(value= {
+    		@ApiResponse(responseCode = "200", description = "Success"),
+    		@ApiResponse(responseCode = "404", description = "Not found")
     })
     public Response getEnvironment() throws Exception {
         try {
