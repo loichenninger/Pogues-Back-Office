7a339d4b1ba8cdc7f652c1ccb06ee8b08a300057
Loïc Henninger <loic.henninger@insee.fr>
2020-12-02 13:46:42 +0100

    Update metadata endpoints for openApi 3.0

--- src/main/java/fr/insee/pogues/webservice/rest/PoguesMetadata.java
+++ src/main/java/fr/insee/pogues/webservice/rest/PoguesMetadata.java
@@ -26,9 +26,12 @@ import fr.insee.pogues.metadata.service.MetadataService;
 import fr.insee.pogues.transforms.DDIToXML;
 import fr.insee.pogues.transforms.PipeLine;
 import fr.insee.pogues.transforms.XMLToJSON;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
  * Main WebService class of the MetaData service
@@ -36,7 +39,7 @@ import io.swagger.annotations.ApiParam;
  * @author I6VWID
  */
 @Path("/meta-data")
-@Api(value = "Pogues MetaData API")
+@Tag(name = "Pogues MetaData API")
 public class PoguesMetadata {
 
 	final static Logger logger = LogManager.getLogger(PoguesMetadata.class);
@@ -53,7 +56,12 @@ public class PoguesMetadata {
 	@GET
 	@Path("item/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "Gets the item with id {id}", notes = "Get an item from Colectica Repository, given it's {id}", response = ColecticaItem.class)
+	@Operation(
+			operationId="getItem",
+			summary = "Gets the item with id {id}",
+			description = "Get an item from Colectica Repository, given it's {id}",
+			responses = { @ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(implementation = ColecticaItem.class)))}
+			)
 	public Response getItem(@PathParam(value = "id") String id) throws Exception {
 		try {
 			ColecticaItem item = metadataService.getItem(id);
@@ -67,9 +75,14 @@ public class PoguesMetadata {
 	@GET
 	@Path("item/{id}/refs/")
 	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "Get the children refs with parent id {id}", notes = "This will give a list of object containing a reference id, version and agency. Note that you will"
+	@Operation(
+			operationId="getChildrenRef",
+			summary = "Get the children refs with parent id {id}",
+			description = "This will give a list of object containing a reference id, version and agency. Note that you will"
 			+ "need to map response objects keys to be able to use it for querying items "
-			+ "(see /items doc model)", response = ColecticaItemRefList.class)
+			+ "(see /items doc model)",
+			responses = { @ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(implementation = ColecticaItemRefList.class)))}
+			)
 	public Response getChildrenRef(@PathParam(value = "id") String id) throws Exception {
 		try {
 			ColecticaItemRefList refs = metadataService.getChildrenRef(id);
@@ -83,7 +96,12 @@ public class PoguesMetadata {
 	@GET
 	@Path("units")
 	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "Get units measure", notes = "This will give a list of objects containing the uri and the label for all units", response = Unit.class, responseContainer = "List")
+	@Operation(
+			operationId = "getUnits",
+			summary = "Get units measure", 
+			description = "This will give a list of objects containing the uri and the label for all units",
+			responses = { @ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(type = "List", implementation = Unit.class)))}
+			)
 	public Response getUnits() throws Exception {
 		try {
 			List<Unit> units = metadataService.getUnits();
@@ -98,9 +116,14 @@ public class PoguesMetadata {
 	@Path("items")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "Get all de-referenced items", notes = "Maps a list of ColecticaItemRef given as a payload to a list of actual full ColecticaItem objects", response = ColecticaItem.class, responseContainer = "List")
+	@Operation(
+			operationId = "getItems",
+			summary = "Get all de-referenced items", 
+			description = "Maps a list of ColecticaItemRef given as a payload to a list of actual full ColecticaItem objects",
+			responses = { @ApiResponse(content = @Content(mediaType = "application/json", schema = @Schema(type = "List", implementation = ColecticaItem.class)))}
+			)
 	public Response getItems(
-			@ApiParam(value = "Item references query object", required = true) ColecticaItemRefList query)
+			@Parameter(description = "Item references query object", required = true) ColecticaItemRefList query)
 			throws Exception {
 		try {
 			List<ColecticaItem> children = metadataService.getItems(query);
@@ -114,7 +137,11 @@ public class PoguesMetadata {
 	@GET
 	@Path("item/{id}/ddi")
 	@Produces(MediaType.APPLICATION_XML)
-	@ApiOperation(value = "Get DDI document", notes = "Gets a full DDI document from Colectica repository reference {id}", response = String.class)
+	@Operation(
+			operationId = "getFullDDI",
+			summary = "Get DDI document", 
+			description = "Gets a full DDI document from Colectica repository reference {id}" 
+			/*,response = String.class*/)
 	public Response getFullDDI(@PathParam(value = "id") String id) throws Exception {
 		try {
 			String ddiDocument = metadataService.getDDIDocument(id);
@@ -132,121 +159,14 @@ public class PoguesMetadata {
 		}
 	}
 
-	//
-	// @GET
-	// @Path("questionnaire/{id}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getQuestionnaire",
-	// notes = "Gets the questionnaire with id {id}",
-	// response = String.class)
-	// public Response getQuestionnaire(@PathParam(value = "id") String id)
-	// throws Exception {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-
-	// @POST
-	// @Path("questionnaire")
-	// @Consumes(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "pubishQuestionnaire",
-	// notes = "Puts the questionnaire : create a new questionnaire or a new
-	// version of the questionnaire on the repository",
-	// response = String.class)
-	// public Response pubishQuestionnaire() {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-	//
-	// @GET
-	// @Path("questionnaires/owner/{owner}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getQuestionnairesByOwner",
-	// notes = "Gets the questionnaires list with owner {owner}",
-	// response = String.class)
-	// public Response getQuestionnairesByOwner(@PathParam(value = "owner")
-	// String owner) {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-
-	// @GET
-	// @Path("questionnaires/label/{label}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getQuestionnairesByLabel",
-	// notes = "Gets questionnaires list with label {label}",
-	// response = String.class)
-	// public Response getQuestionnairesByLabel(@PathParam(value = "label")
-	// String label) {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-
-	// @GET
-	// @Path("sequence/{id}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getSequenceOrSubSequence",
-	// notes = "Gets the Sequence or SubSequence with id {id}",
-	// response = String.class)
-	// public Response getSequenceOrSubSequence(@PathParam(value = "id") String
-	// id) {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-
-	// @GET
-	// @Path("sequences/owner/{owner}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getSequencesOrSubSequencesByOwner",
-	// notes = "Gets Sequences or SubSequences list with owner {owner}",
-	// response = String.class)
-	// public Response getSequencesOrSubSequencesByOwner(@PathParam(value =
-	// "owner") String owner) {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-
-	// @GET
-	// @Path("sequences/label/{label}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getSequencesOrSubSequencesByLabel",
-	// notes = "Gets Sequences or SubSequences list with label {label}",
-	// response = String.class)
-	// public Response getSequencesOrSubSequencesByLabel(@PathParam(value =
-	// "label") String label) {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-
-	// @GET
-	// @Path("question/{id}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getQuestion",
-	// notes = "Gets the question with id {id}",
-	// response = String.class)
-	// public Response getQuestion(@PathParam(value = "id") String id) {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-
-	// @GET
-	// @Path("questions/owner/{owner}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getQuestionsByOwner",
-	// notes = "Gets questions list with owner {owner}",
-	// response = String.class)
-	// public Response getQuestionsByOwner(@PathParam(value = "owner") String
-	// owner) {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-
-	// @GET
-	// @Path("questions/label/{label}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getQuestionsByLabel",
-	// notes = "Gets questions list with label {label}",
-	// response = String.class)
-	// public Response getQuestionsByLabel(@PathParam(value = "label") String
-	// label) {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-	//
-
 	@GET
 	@Path("code-list/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "getCodeList", notes = "Gets the code-list with id {id}", response = String.class)
+	@Operation(
+			operationId = "getCodeList",
+			summary = "getCodeList", 
+			description = "Gets the code-list with id {id}"
+			/*,response = String.class*/)
 	public Response getCodeList(@PathParam(value = "id") String id) throws Exception {
 		String codeList = metadataService.getCodeList(id);
 		PipeLine pipeline = new PipeLine();
@@ -267,65 +187,5 @@ public class PoguesMetadata {
 			throw e;
 		}
 	}
-
-	//
-	// @GET
-	// @Path("codesLists/owner/{owner}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getCodesListsByOwner",
-	// notes = "Gets codesLists list with owner {owner}",
-	// response = String.class)
-	// public Response getCodesListsByOwner(@PathParam(value = "owner") String
-	// owner) {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-	//
-	// @GET
-	// @Path("codesLists/label/{label}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getCodesListsByLabel",
-	// notes = "Gets codesLists list with label {label}",
-	// response = String.class)
-	// public Response getCodesListsByLabel(@PathParam(value = "label") String
-	// label) {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-	//
-	//
-	// @GET
-	// @Path("mutualizedSequencesOrSubSequences/label/{label}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getMutualizedSequencesOrSubSequencesByLabel",
-	// notes = "Gets Mutualized Sequences or SubSequences list with label
-	// {label}",
-	// response = String.class)
-	// public Response
-	// getMutualizedSequencesOrSubSequencesByLabel(@PathParam(value = "label")
-	// String label) {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-	//
-	//
-	// @GET
-	// @Path("mutualizedCodesLists/label/{label}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getMutualizedCodesListsByLabel",
-	// notes = "Gets Mutualized CodesLists list with label {label}",
-	// response = String.class)
-	// public Response getMutualizedCodesListsByLabel(@PathParam(value =
-	// "label") String label) {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
-	//
-	// @GET
-	// @Path("mutualizedQuestion/label/{label}")
-	// @Produces(MediaType.APPLICATION_JSON)
-	// @ApiOperation(value = "getMutualizedQuestionsByLabel",
-	// notes = "Gets Mutualized Questions list with label {label}",
-	// response = String.class)
-	// public Response getMutualizedQuestionsByLabel(@PathParam(value = "label")
-	// String label) {
-	// return Response.status(Status.NOT_IMPLEMENTED).build();
-	// }
 
 }
