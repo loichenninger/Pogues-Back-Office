26372bab04a68075df37b4efad8c25ae70245dc8
Loïc Henninger <loic.henninger@insee.fr>
2020-12-02 16:37:07 +0100

    Adding description for documentation

--- src/main/java/fr/insee/pogues/metadata/model/ColecticaItem.java
+++ src/main/java/fr/insee/pogues/metadata/model/ColecticaItem.java
@@ -2,69 +2,63 @@ package fr.insee.pogues.metadata.model;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-/**
- * {
- "VersionRationale": {
- "property1": "string",
- "property2": "string"
- },
- "ItemType": "string",
- "AgencyId": "string",
- "Version": 0,
- "Identifier": "string",
- "Item": "string",
- "Notes": [
- {}
- ],
- "VersionDate": "2017-09-14T09:12:41Z",
- "VersionResponsibility": "string",
- "IsPublished": true,
- "IsDeprecated": true,
- "IsProvisional": true,
- "ItemFormat": "string"
- }
- */
 public class ColecticaItem {
 
+	@Schema(description = "ItemType")
     @JsonProperty("ItemType")
     public String itemType;
 
+	@Schema(description = "AgencyId")
     @JsonProperty("AgencyId")
     public String agencyId;
 
+	@Schema(description = "Version")
     @JsonProperty("Version")
     public String version;
 
+	@Schema(description = "Identifier")
     @JsonProperty("Identifier")
     public String identifier;
 
+	@Schema(description = "DDI representation of the item")
     @JsonProperty("Item")
     public String item;
 
+	@Schema(description = "VersionDate")
     @JsonProperty("VersionDate")
     public String versionDate;
 
+	@Schema(description = "VersionResponsibility")
     @JsonProperty("VersionResponsibility")
     public String versionResponsibility;
 
+	@Schema(description = "IsPublished")
     @JsonProperty("IsPublished")
     public boolean isPublished;
 
+	@Schema(description = "IsDeprecated")
     @JsonProperty("IsDeprecated")
     public boolean isDeprecated;
 
+	@Schema(description = "IsProvisional")
     @JsonProperty("IsProvisional")
     public boolean isProvisional;
 
+	@Schema(description = "ItemFormat")
     @JsonProperty("ItemFormat")
     public String itemFormat;
 
+	@Schema(description = "VersionRationale")
     @JsonProperty("VersionRationale")
     public JSONObject versionRationale;
 
+	@Schema(description = "Notes")
     @JsonProperty("Notes")
     public JSONArray notes;
 
