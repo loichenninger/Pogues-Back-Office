4a58125673008acb8cc4375f6ac150579d4949a8
Ophélie Rogel <ophelie.rogel@insee.fr>
2021-06-10 17:59:51 +0200

    adding xml parsing into xpathvtl parsing

--- src/main/java/fr/insee/pogues/webservice/rest/PoguesUtil.java
+++ src/main/java/fr/insee/pogues/webservice/rest/PoguesUtil.java
@@ -1,44 +1,45 @@
 package fr.insee.pogues.webservice.rest;
 
 import static fr.insee.pogues.utils.json.XPathVTL.parseToVTL;
-import fr.insee.pogues.conversion.JSONDeserializer;
-import fr.insee.pogues.model.Questionnaire;
-import fr.insee.pogues.user.model.User;
-import fr.insee.pogues.user.service.UserService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import fr.insee.pogues.transforms.JSONToXML;
+import fr.insee.pogues.transforms.XMLToJSON;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @Component
 @Path("/util")
 
 public class PoguesUtil {
 
+	@Autowired
+	JSONToXML jSONToXML;
+	
+	@Autowired
+	XMLToJSON xMLToJSON;
 
     @Context
     HttpServletRequest request;
 
-    @Autowired
-    private UtilService utilService;
 
     final static Logger logger = LogManager.getLogger(PoguesUtil.class);
 
@@ -56,18 +57,40 @@ public class PoguesUtil {
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Parsed"),
     })
-    public Response parseQuestionnaireXptah2VTL(@RequestBody JSONObject json) throws Exception {
+    public String parseQuestionnaireXptah2VTL(@RequestBody String json) throws Exception {
         try {
-        	JSONObject jsonParsed;
-        	Object o=json.compute("Expression", (key,value) -> parseToVTL(value.toString())); 
-            return null;
+        	
+        	String xml = jSONToXML.transform(json, null, null);
+        	
+        	String possibleNodes = "(Expression|Formula|Minimum|Maximum)";
+    		Pattern pattern = Pattern.compile("(<"+possibleNodes+">)((.)*?)(</"+possibleNodes+">)");
+
+    		Matcher matcher = pattern.matcher(xml);
+    		StringBuffer stringBuffer = new StringBuffer();
+    		while(matcher.find()){
+    			//matcher.group(0)=all expression
+    			//matcher.group(1)=start of xml node ex:<label>
+    			//matcher.group(2)=name of xml node ex:label
+    			//matcher.group(3)=content of xml node
+    			//matcher.group(4)=last char in group(3) ?
+    			//matcher.group(5)=end of xml node ex:</label>
+    			//matcher.group(6)=name of xml node ex:label
+    			String replacement = matcher.group(1) + parseToVTL(matcher.group(3)) + matcher.group(5);
+    			matcher.appendReplacement(stringBuffer,"");
+    			stringBuffer.append(replacement);
+    		}
+    		matcher.appendTail(stringBuffer);
+    		stringBuffer.toString();
+        	
+    		String jsonf = xMLToJSON.transform(xml, null, null);
+        	return jsonf;
         } catch (PoguesException e) {
             logger.error(e.getMessage(), e);
             throw e;
         }
+        
     }
 
 
 
 }
-.
